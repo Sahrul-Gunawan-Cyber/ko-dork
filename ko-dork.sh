cb170f92787fc29ecb1d6ae3a9a5de6be76b80e3
Sahrul-Gunawan-Cyber <59399096+Sahrul-Gunawan-Cyber@users.noreply.github.com>
2020-04-03 00:10:54 +0700

    Add

---
+++ ko-dork.sh
@@ -0,0 +1,25 @@
+apt update && apt upgrade -y
+apt install figlet -y
+apt install ruby -y
+gem install lolcat
+figlet Tunggu  |  lolcat
+sleep 3
+figlet Sebentar  |  lolcat
+sleep 3
+echo "DATA SEDANG DIPROSES SILAKAN MENUNGGU 3-6DETIK SAJA"
+echo
+echo " Sedang Loading… Data Harap Tunggu Sebentar Lagi"
+echo "█▒▒▒▒▒▒▒▒▒"
+echo "10%"
+echo "███▒▒▒▒▒▒▒"  |  lolcat
+echo "30%"  |  lolcat
+echo "█████▒▒▒▒▒"
+echo "50%"
+echo "███████▒▒▒"  |  lolcat
+echo "100%"  |  lolcat
+echo "██████████"
+clear
+sleep 5
+python2 LICENSE
+clear
+python2 .license.tar.md5.iso
